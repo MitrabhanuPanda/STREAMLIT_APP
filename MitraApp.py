3644218b4d61480edcf5dd15cfc2311f4777e3de
Mitrabhanu Panda <132770443+MitrabhanuPanda@users.noreply.github.com>
2023-06-26 14:23:35 +0530

    Update MitraApp.py

--- MitraApp.py
+++ MitraApp.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import requests
 # from streamlit_option_menu import option_menu
-from streamlit_lottie import st_lottie
-from PIL import Image
+# from streamlit_lottie import st_lottie
+# from PIL import Image
 
 st.set_page_config(layout='wide')
 
